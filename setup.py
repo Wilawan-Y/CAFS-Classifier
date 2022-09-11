ea1d23b9a898123b7f173cbc6c35975987403cf8
wilawan <56219991+Wilawan-Y@users.noreply.github.com>
2022-09-11 21:36:01 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(name='NE_Extracter',
       version='1.0.0',
@@ -7,5 +7,11 @@ setup(name='NE_Extracter',
       author_email='wilawan_y@cmu.ac.th',
       license='gnu',
       url="https://github.com/Wilawan-Y/EconomicFinancial-NE-Extractor",
+      install_requires=[
+          'nltk',
+          'marisa_trie==0.7.7',
+          're',
+          'pickle',
+      ],
 	  packages=find_packages()
       )
