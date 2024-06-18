150c78c7333b8dca92245915c0b8f52fc0dd9266
wilawan <56219991+Wilawan-Y@users.noreply.github.com>
2024-06-18 21:16:15 +0700

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from CAFS import Extraction, Classify
 import pickle
-import operator
+import warnings
+warnings.filterwarnings("ignore")
 
 def tokenize(text):
     return text.split(',')
@@ -10,17 +11,13 @@ remove_key = pickle.load(open('remove.list', 'rb'))
 vectorizer = pickle.load(open('vectorizer.vec', 'rb'))
 model = pickle.load(open('svm.model', 'rb'))
 
-text = 'biden budget nominee absolutely backs u.s. minimum wage hike'
-#text = 'U.S. coffee roasters weigh price increases, cite shipping inflation'
+text = 'U.S. coffee roasters weigh price increases, cite shipping inflation'
 Tag = Extraction(text,NE_dict,remove_key)
 Tag.tagging()
 
 print("Word Tokenization: ", Tag.get_tokens())
 print("Entity Keyword: ", Tag.get_keys())
 print("Entity Tagged: ", Tag.get_NEtag())
-#tokens : ['biden', 'budget', 'nominee', 'absolutely', 'backs', 'u.s.', 'minimum wage', 'hike']
-#keys: ['biden', 'u.s.', 'minimum wage']
-#NE tag: ['PER', 'GPE', 'IND']
 
 X = vectorizer.transform(Tag.get_tokens())
 print("SVM Baseline Clssification Result: ", model.predict(X)[0])
